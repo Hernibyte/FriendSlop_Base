6cd73c68a69506e1a066198a342c9bf2da6c16d3
Hernan <gossa.hernan@gmail.com>
2026-02-22 20:07:58 -0300

    Initial project

---
+++ Source/FriendSlop_Base/FriendSlop_Base.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FriendSlop_Base.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FriendSlop_Base, "FriendSlop_Base" );
